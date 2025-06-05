766ac43ce77786008f08787ca77dceaf4902a3ba
Huynh Duong <huynh.dh2310@gmail.com>
2025-06-05 16:49:12 +0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,17 @@ set -g fish_greeting
 
 set -x GOPATH $HOME/Developer/Packages/go
 
-fish_add_path -p $GOPATH/bin $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin $HOME/.local/bin
+fish_add_path -p $GOPATH/bin $HOME/.cargo/bin $HOME/.local/share/bob/nvim-bin $HOME/.local/bin $HOME/.dotnet/tools
 
 set -x EDITOR nvim
+
+set -x GTK_IM_MODULE ""
+set -x QT_IM_MODULE ""
+set -x SDL_IM_MODULE ""
+
+
+set -g fish_history_max 10000
+set -g fish_history_save_on_exit 1
 
 zoxide init fish | source
 starship init fish | source
@@ -25,9 +33,18 @@ function ls --wraps eza
     eza --icons $argv
 end
 
+function lg --wraps lazygit
+    lazygit $argv
+end
+
+function ld --wraps lazydocker
+    lazydocker $argv
+end
+
 function random_quote
-  set animal (random choice {default,dragon,dragon-and-cow,elephant,moose,stegosaurus,tux,vader,www})
-  fortune -s | cowsay -f $animal -s
+    set animal (random choice {default,dragon,dragon-and-cow,elephant,moose,stegosaurus,tux,vader,www})
+    fortune -s | cowsay -f $animal -s
 end
 
 random_quote
+
